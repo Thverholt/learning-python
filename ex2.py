ec8f64821486fd34cff0863a94c233d9cbfa8c68
Thverholt <64042450+Thverholt@users.noreply.github.com>
2020-05-09 14:04:09 -0700

    we did exercise 2.

---
+++ ex2.py
@@ -0,0 +1,7 @@
+# a comment, so you can read your program later.
+# anything after the # is ignored by python.
+print("I could have code like this.") # and the comment after is ignored
+# you can also use a comment to "disable" or comment out code:
+# print ("this won't run.")
+
+print ("this will run.")
